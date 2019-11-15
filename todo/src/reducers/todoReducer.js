957647884137d7545f682668436d4514a4e50253
DentonWarnock <dentonwarnock@gmail.com>
2019-11-15 15:53:47 -0800

    Added initial state and todoReducer.

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        case 'CHANGE_TODO':
+            return {...state, todo: action.payload}
+        default:
+            return state;
+    }
+         
+}
